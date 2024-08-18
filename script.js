2f3e2bd78e6e7278a8504e43a00be15a603fbfc0
Ajeet Kumar <98801990+16ajeet@users.noreply.github.com>
2024-08-18 15:10:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const btn = document.querySelector("#btn");
+const p = document.querySelector("p")
+
+const changeText = () => {
+	// const h1 = document.createElement('h1');
+	// h1.textContent = "Entered Metaverse"
+	// p.appendChild(h1);
+
+	p.innerHTML = "<h1>Entered Metaverse</h1>"
+	
+}
